22f6b9375574718e3af348cb0346b802c0cdf819
Zandgall <Alexanderdgall@gmail.com>
2024-11-03 14:45:42 -0600

    Release 1

--- src/com/zandgall/csc422/a1/Main.java
+++ src/com/zandgall/csc422/a1/Main.java
@@ -1,15 +1,41 @@
 package com.zandgall.csc422.a1;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 	private static ArrayList<Pet> pets = new ArrayList<>();
+	private static Scanner in;
 
-	public static void main(String[] args){ 
-		pets.add(new Pet(0, "Kitty", 9));
-		pets.add(new Pet(1, "Bruno", 13));
-		pets.add(new Pet(2, "Boomer", 8));
+	public static void main(String[] args){
+		pets.add(new Pet("Kitty", 9));
+		pets.add(new Pet("Bruno", 13));
+		pets.add(new Pet("Boomer", 8));
 		printTable();
+		
+		while(true) {
+			System.out.printf("Pet Database%nl) List Pets%na) Add Pet%ne) exit%n> ");
+			in = new Scanner(System.in);
+			String choice = in.nextLine().toLowerCase().strip();
+			switch(choice.charAt(0)) {
+				case 'a':
+					addPet();
+					break;
+				case 'l':
+					printTable();
+					break;
+				case 'e':
+					return;
+			}
+		}
+	}
+
+	public static void addPet() {
+		System.out.printf("Name: ");
+		String name = in.nextLine();
+		System.out.printf("Age: ");
+		int age = in.nextInt();
+		pets.add(new Pet(name, age));
 	}
 
 	public static void printTable() {
@@ -29,8 +55,8 @@ public class Main {
 
 		System.out.println(split);
 		
-		for(Pet p : pets)
-			System.out.printf("|%-3d|%-"+max_name_length+"s|%-3d|%n", p.id, p.name, p.age);
+		for(int i = 0; i < pets.size(); i++)
+			System.out.printf("|%-3d|%-"+max_name_length+"s|%-3d|%n", i, pets.get(i).name, pets.get(i).age);
 	
 		System.out.println(split);
 
@@ -40,10 +66,9 @@ public class Main {
 
 	private static class Pet {
 		protected String name;
-		protected int id, age;
+		protected int age;
 
-		public Pet(int id, String name, int age) {
-			this.id = id;
+		public Pet(String name, int age) {
 			this.name = name;
 			this.age = age;
 		}
